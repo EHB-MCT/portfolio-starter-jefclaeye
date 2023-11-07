d8c902eae50d151bcc6a90663ea911e01bfbf858
jefclaeye <jef.claeye@student.ehb.be>
2023-11-07 13:33:06 +0100

    Build: docker build first image

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const studentsRoutes = require('./routes/studentsRoutes'); // Import your route module
 const projectsRoutes = require('./routes/projectsRoutes'); // Import your route module
-const port = process.env.PORT || 3000;
+
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send({ message: "hello" })
+})
 
 // Use the studentsRoutes module for the "/api/students" endpoint
 app.use('/api/students', studentsRoutes);
 app.use('/api/projects', projectsRoutes);
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(3000, (err) => {
+    if (!err) {
+        console.log(`Server is running on port 3000`)
+    } else { console.error(err) };
 });
