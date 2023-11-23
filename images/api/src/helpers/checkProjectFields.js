c001f2d7e7e547397eb4e2006310d86f7f1d456c
jefclaeye <jef.claeye@student.ehb.be>
2023-11-23 15:16:25 +0100

    feat: documentation for crud to help jest test

--- images/api/src/helpers/checkProjectFields.js
+++ images/api/src/helpers/checkProjectFields.js
@@ -1,9 +1,10 @@
+/**
+ * Checks if the provided project name meets certain criteria.
+ * @param {string} name - Name of the project to be checked.
+ * @returns {boolean} - Returns true if the project name is a string with a length greater than 2, otherwise returns false.
+ */
 function checkProjectFields(name) {
-    return typeof name === 'string' && name.length > 2; // Assuming a valid name should have at least 3 characters
+    return typeof name === 'string' && name.length > 2;
 }
-
-
-
-
 
 module.exports = checkProjectFields;
